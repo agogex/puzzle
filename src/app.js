064951fd5db93aba00fb87fb177863bc0486cc31
agogex <agoge@outlook.com>
2017-11-29 12:34:00 +0200

    Done puzzle

--- src/app.js
+++ src/app.js
@@ -20,54 +20,23 @@ window.onload = function () {
   userForm.addEventListener('submit', event => {
     event.preventDefault();
     const input = document.querySelector('#username');
-    username = input.value;
+    username = input.value.toLowerCase();
     counter = new Date();
-    timer = setInterval(() => {
-      const now = new Date();
-      const time = now - counter;
-      const _counter = document.querySelector('#counter');
-      let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-      let seconds = Math.floor((time % (1000 * 60)) / 1000);
-      minutes = minutes <= 9 ? `0${minutes}` : minutes;
-      seconds = seconds <= 9 ? `0${seconds}` : seconds;
-      _counter.innerHTML = `${minutes}:${seconds}`;
-      score = time;
-    }, 1000);
+    timer = setInterval(processTimer, 1000);
     document.querySelector('#name').innerHTML = username;
-    document.querySelector('#name').innerHTML = username;
-    hide(document.querySelector('#user_form'));
+    hide(userForm);
     show(document.querySelector('#user_info'));
     interact('.draggable').draggable(true);
   });
 
-  imgLoad(img).then((loadedImage) => {
-    for (let i = 0, x = 0, y = 0; i < 9; i++ , x += 130) {
-      if (i !== 0 && i % 3 === 0) {
-        x = 0;
-        y += 130;
-      }
-      const canvas = document.createElement('canvas');
-      canvas.height = 130;
-      canvas.width = 130;
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(loadedImage, x, y, 130, 130, 0, 0, 130, 130);
-      imgUrls.push(canvas.toDataURL());
-    }
-    pieces = imgUrls.length;
-  }).then(() => {
-    const dropzoneWrapper = document.querySelector('#dropzone_wrapper');
-    for (let i = 0; i < 9; i++) {
-      const dropzone = document.createElement('div');
-      dropzone.classList.add('dropzone');
-      dropzone.setAttribute('id', `dropzone-${i}`);
-      dropzone.style.background = `url(${imgUrls[i]}) center center no-repeat`;
-      dropzone.style.backgroundSize = `cover`;
-      dropzoneWrapper.appendChild(dropzone);
-    }
-  }).then(() => initDropzones(pieces)).then(() => initDragzones(imgUrls));
+  imgLoad(img)
+    .then((loadedImage) => processImage(loadedImage, imgUrls))
+    .then(() => createDropzones(imgUrls))
+    .then(() => initDropzones())
+    .then(() => initDragzones(imgUrls));
 
-  const save = document.querySelector('#save');
-  save.addEventListener('click', saveScore);
+  document.querySelector('#save').addEventListener('click', saveScore);
+  document.querySelector('#restart').addEventListener('click', () => restartGame(imgUrls));
 }
 
 function imgLoad(img) {
@@ -76,6 +45,22 @@ function imgLoad(img) {
       resolve(this);
     };
   });
+}
+
+function processImage(loadedImage, imgUrls) {
+  for (let i = 0, x = 0, y = 0; i < 9; i++ , x += 130) {
+    if (i !== 0 && i % 3 === 0) {
+      x = 0;
+      y += 130;
+    }
+    const canvas = document.createElement('canvas');
+    canvas.height = 130;
+    canvas.width = 130;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(loadedImage, x, y, 130, 130, 0, 0, 130, 130);
+    imgUrls.push(canvas.toDataURL());
+  }
+  pieces = imgUrls.length;
 }
 
 function initDragzones(imgUrls) {
@@ -107,6 +92,22 @@ function initDragzones(imgUrls) {
     setCoordinates(target, x, y);
   }
 
+  createDraggable(imgUrls);
+}
+
+function createDropzones(imgUrls) {
+  const dropzoneWrapper = document.querySelector('#dropzone_wrapper');
+  for (let i = 0; i < 9; i++) {
+    const dropzone = document.createElement('div');
+    dropzone.classList.add('dropzone');
+    dropzone.setAttribute('id', `dropzone-${i}`);
+    dropzone.style.background = `url(${imgUrls[i]}) center center no-repeat`;
+    dropzone.style.backgroundSize = `cover`;
+    dropzoneWrapper.appendChild(dropzone);
+  }
+}
+
+function createDraggable(imgUrls) {
   const dragzoneWrapper = document.querySelector('#dragzone_wrapper');
   for (let i = 0; i < 9; i++) {
     const draggable = document.createElement('div');
@@ -122,6 +123,16 @@ function initDragzones(imgUrls) {
   }
 }
 
+function eraseDropzones() {
+  const dropzoneWrapper = document.querySelector('#dropzone_wrapper');
+  dropzoneWrapper.innerHTML = '';
+}
+
+function eraseDraggable() {
+  const dragzoneWrapper = document.querySelector('#dragzone_wrapper');
+  dragzoneWrapper.innerHTML = '';
+}
+
 function setCoordinates(element, x, y) {
   // translate the element
   element.style.webkitTransform =
@@ -133,7 +144,7 @@ function setCoordinates(element, x, y) {
   element.setAttribute('data-y', y);
 }
 
-function initDropzones(pieces) {
+function initDropzones() {
   const dropzones = document.querySelectorAll('.dropzone');
 
   for (let dropzone of dropzones) {
@@ -148,10 +159,6 @@ function initDropzones(pieces) {
         event.relatedTarget.classList.remove('dropped');
         event.target.classList.remove('active');
       },
-      ondragenter: function (event) {
-      },
-      ondragleave: function (event) {
-      },
       ondrop: function (event) {
         event.relatedTarget.classList.add('dropped');
         event.target.classList.add('active');
@@ -159,11 +166,36 @@ function initDropzones(pieces) {
         if(pieces === 0) {
           puzzleDone(timer);
         }
-      },
-      ondropdeactivate: function (event) {
       }
     });
   }
+}
+
+function processTimer() {
+  const now = new Date();
+  const time = now - counter;
+  const _counter = document.querySelector('#counter');
+  _counter.innerHTML = normalizeScore(time);
+  score = time;
+}
+
+function normalizeScore(time) {
+  let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((time % (1000 * 60)) / 1000);
+  minutes = minutes <= 9 ? `0${minutes}` : minutes;
+  seconds = seconds <= 9 ? `0${seconds}` : seconds;
+  return `${minutes}:${seconds}`;
+}
+
+function restartGame(imgUrls) {
+  eraseDropzones();
+  createDropzones(imgUrls);
+  eraseDraggable();
+  createDraggable(imgUrls);
+  hide(document.querySelector('#puzzle_done'));
+  pieces = imgUrls.length;
+  counter = new Date();
+  timer = setInterval(processTimer, 1000);
 }
 
 function puzzleDone(timer) {
@@ -187,9 +219,29 @@ function show(element) {
   element.style.height = 'inherit';
 }
 
-function saveScore () {
-  return axios.post(`http://localhost:3000/users`, {
+function saveScore() {
+  axios.post(`http://localhost:3000/users`, {
     name: username,
     score
-  })
+  }).then(getTopScores)
+    .then(showTopScores);
+}
+
+function showTopScores(result) {
+  const scores = result.data;
+  if (scores.length > 0) {
+    const topScores = document.querySelector('#top_scores');
+    for (let i = 0; i < scores.length; i++) {
+      const div = document.createElement('div');
+      div.innerHTML = `${i + 1}. ${scores[i].name.toUpperCase()} - ${normalizeScore(scores[i].score)}`;
+      topScores.appendChild(div);
+    }
+    hide(document.querySelector('#user_info'));
+    hide(document.querySelector('#puzzle_done'));
+    show(topScores);
+  }
+}
+
+function getTopScores() {
+  return axios.get(`http://localhost:3000/users?_sort=score&_order=asc&_limit=10`);
 }
